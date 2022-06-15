5a9536c1b8593fa99b760129b3e4081e2ab92b3d
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-06-15 20:34:05 +0300

    Work on Autocheck Module-2 #2

--- js/ac_m-02.js
+++ js/ac_m-02.js
@@ -6,7 +6,7 @@
 
 // function checkAge(age) {
 //     if (age >= 18) { // Change this line
-//     return "You are an adult";
+//         return "You are an adult";
 //     }
 
 //     return "You are a minor";
@@ -71,22 +71,179 @@
 
 
 /*
-* - Задача 2 
+* - Задача 2 (исходный вариант)
 ! ========================================
  */
 
-function checkPassword(password) {
-    const ADMIN_PASSWORD = "jqueryismyjam";
+// function checkPassword(password) {
+//     const ADMIN_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+
+//     let message;
+
+//     if (password === ADMIN_PASSWORD) {
+//     message = "Welcome!";
+//     } else {
+//     message = "Access denied, wrong password!";
+//     }
+//     console.log(message); // ! Delete
+//     return message;
+//   // Change code above this line
+// }
+
+// checkPassword("mangohackzor");
+// checkPassword("polyhax");
+// checkPassword("jqueryismyjam");
+
+
+/*
+* - Задача 2 (ОТВЕТ)
+! ========================================
+ */
+
+// function checkPassword(password) {
+//     const ADMIN_PASSWORD = "jqueryismyjam";
+
+//     if (password === ADMIN_PASSWORD) {
+//         return "Welcome!";
+//     }
+    
+//     return "Access denied, wrong password!";
+// }
+
+// console.log(checkPassword("mangohackzor"));
+// console.log(checkPassword("polyhax"));
+// console.log(checkPassword("jqueryismyjam"));
+
+
+/*
+* - Задача 3 (исходный вариант)
+! ========================================
+ */
+
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+//     let message;
+
+//     if (ordered === 0) {
+//     message = "Your order is empty!";
+//     } else if (ordered > available) {
+//     message = "Your order is too large, not enough goods in stock!";
+//     } else {
+//     message = "The order is accepted, our manager will contact you";
+//     }
+
+//     return message;
+//   // Change code above this line
+// }
+
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(70, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+// console.log(checkStorage(150, 0));
+
+
+/*
+* - Задача 3 (ОТВЕТ)
+! ========================================
+ */
+
+// function checkStorage(available, ordered) {
+//     if (ordered === 0) {
+//         return "Your order is empty!";
+//     } if (ordered > available) {
+//         return "Your order is too large, not enough goods in stock!";
+//     } 
+//     return "The order is accepted, our manager will contact you";
+// }
+
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(70, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+// console.log(checkStorage(150, 0));
+
+
+/*
+* - Задача 4 (ОТВЕТ)
+! ========================================
+ */
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+
+/*
+* - Задача 5 (ОТВЕТ)
+! ========================================
+ */
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// // Change code below this line
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[fruits.length - 1];
+
+// //  Эту часть на принимает, её писать не надо:
+// // console.log(firstElement);
+// // console.log(secondElement);
+// // console.log(lastElement);
+
+
+/*
+* - Задача 6 (ОТВЕТ)
+! ========================================
+ */
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// // Write your code under this line
+// fruits[1] = "peach";
+// fruits[3] = "banana";
+// console.log(fruits); 
+
+
+/*
+* - Задача 7 (ОТВЕТ)
+! ========================================
+ */
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// // Change code below this line
+// const fruitsArrayLength = fruits.length;
+// console.log(fruitsArrayLength);
+
+
+/*
+* - Задача 8 (ОТВЕТ)
+! ========================================
+ */
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// // Change code below this line
+// const lastElementIndex = fruits.length - 1;
+// console.log(lastElementIndex);
+// const lastElement = fruits[lastElementIndex];
+// console.log(lastElement);
+
+
+/*
+* - Задача 9 (ОТВЕТ)
+! ========================================
+ */
+
+function getExtremeElements(array) {
   // Change code below this line
 
-    let message;
 
-    if (password === ADMIN_PASSWORD) {
-    message = "Welcome!";
-    } else {
-    message = "Access denied, wrong password!";
-    }
-
-    return message;
   // Change code above this line
 }
+
+console.log(getExtremeElements([1, 2, 3, 4, 5]));
+console.log(getExtremeElements(["Earth", "Mars", "Venus"]));
+console.log(getExtremeElements(["apple", "peach", "pear", "banana"]));
