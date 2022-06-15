99d00e3b3b5284d4aac2a7e88b76730d33ef2aa3
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-06-16 01:14:19 +0300

    Work on Autocheck Module-2 +18

--- js/ac_m-02.js
+++ js/ac_m-02.js
@@ -269,7 +269,7 @@
 
 
 /*
-* - Задача 10 (ОТВЕТ)
+* - Задача 10 (ОТВЕТ) (1-й вариант) - ПРИНЯТ!!!
 ! ========================================
  */
 
@@ -279,7 +279,7 @@
 //     words = message.split(delimeter);
 //     console.log(words);
 //   // Change code above this line
-//   return words;
+//     return words;
 // }
 
 // splitMessage("Mango hurries to the train", " ");
@@ -287,15 +287,184 @@
 // splitMessage("best_for_week", "_");
 
 
+
 /*
-* - Задача 11 (ОТВЕТ)
+* - Задача 10 (ОТВЕТ) (2-й вариант) - Вероятно тоже будет ПРИНЯТ
 ! ========================================
  */
 
-function calculateEngravingPrice(message, pricePerWord) {
-   // Change code below this line
+// function splitMessage(message, delimeter) {
+//     let words;
+//   // Change code below this line
+//     words = message.split(delimeter);
+//     // console.log(words);
+//   // Change code above this line
+//     return words;
+// }
+
+// console.log(splitMessage("Mango hurries to the train", " "));
+// console.log(splitMessage("Mango", ""));
+// console.log(splitMessage("best_for_week", "_"));
 
 
+/* 
+* - Задача 11 (ОТВЕТ) - ПРИНЯТ!!!
+! ========================================
+ */
 
-   // Change code above this line
-}
+// function calculateEngravingPrice(message, pricePerWord) {
+//    // Change code below this line
+//     let totalPrice;
+//     let arrayEngraving;
+
+//     arrayEngraving = message.split(" ");
+    
+//     totalPrice = arrayEngraving.length * pricePerWord
+
+//     return totalPrice;
+//    // Change code above this line
+// }
+
+// console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
+// console.log(calculateEngravingPrice("JavaScript is in my blood", 20));
+// console.log(calculateEngravingPrice("Web-development is creative work", 40));
+// console.log(calculateEngravingPrice("Web-development is creative work", 20));
+
+
+/* 
+* - Задача 12 (ОТВЕТ)
+! ========================================
+ */
+
+// function makeStringFromArray(array, delimeter) {
+//     let string;
+//   // Change code below this line
+//     string = array.join(delimeter);
+//   // Change code above this line
+//     return string;
+// }
+
+// console.log(makeStringFromArray(["Mango", "hurries", "to", "the", "train"], " "));
+// console.log(makeStringFromArray(["M", "a", "n", "g", "o"], ""));
+// console.log(makeStringFromArray(["top", "picks", "for", "you"], "_"));
+
+
+/* 
+* - Задача 13 (ОТВЕТ)
+! ========================================
+ */
+
+// function slugify(title) {
+//   // Change code below this line
+//     let slug;
+//     let titleToLowerCase = title.toLowerCase()
+
+//     slug = titleToLowerCase.split(" ");
+//     slug = slug.join("-");
+
+//     return slug;
+//   // Change code above this line
+// }
+
+// console.log(slugify("Arrays for begginers"));
+// console.log(slugify("English for developer"));
+// console.log(slugify("Ten secrets of JavaScript"));
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+
+/* 
+* - Задача 14 (ОТВЕТ)
+! ========================================
+ */
+
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// // Change code below this line
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, 4);
+// const lastThreeEls = fruits.slice(-3);
+
+// // console.log(firstTwoEls);
+// // console.log(nonExtremeEls);
+// // console.log(lastThreeEls);
+
+
+/* 
+* - Задача 15 (ОТВЕТ)
+! ========================================
+ */
+
+// const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const newClients = ['Peach', 'Houston'];
+
+// const allClients = oldClients.concat(newClients); // Change this line
+
+// // console.log(allClients);
+
+
+/* 
+* - Задача 16 (ОТВЕТ)
+! ========================================
+ */
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//     // Change code below this line
+//     const makeArrayNew = firstArray.concat(secondArray);
+//     let maxLengthNew = makeArrayNew.length;
+//     maxLengthNew = maxLengthNew > maxLength ? maxLength : maxLengthNew;
+
+//     // console.log(maxLengthNew);
+//     return (makeArrayNew.slice(0, maxLengthNew));
+//     // Change code above this line
+// }
+
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+
+
+/* 
+* - Задача 17 (ОТВЕТ)
+! ========================================
+ */
+
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i += 1) { // Change this line
+//     console.log(i);
+// }
+
+
+/* 
+* - Задача 18 (ОТВЕТ)
+! ========================================
+ */
+
+// function calculateTotal(number) {
+//     // Change code below this line
+//     let numberSum = 0;
+
+//     for (let i = 1; i <= number; i += 1) { 
+//         numberSum = numberSum + i;
+//     }
+
+//     return (numberSum);  
+//     // Change code above this line
+// }
+
+// console.log(calculateTotal(1));
+// console.log(calculateTotal(3));
+// console.log(calculateTotal(7));
+// console.log(calculateTotal(18));
+// console.log(calculateTotal(24));
+
+
+/* 
+* - Задача 19 (ОТВЕТ)
+! ========================================
+ */
+
