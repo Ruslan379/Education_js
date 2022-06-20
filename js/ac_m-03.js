6bc3a44fcca98ad31acce8b92908a32742d121c9
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-06-20 17:56:23 +0300

    Work on Autocheck Module-3 +11

---
+++ js/ac_m-03.js
@@ -0,0 +1,313 @@
+/*
+* - Autocheck Module -3
+* - Задача 1 (ПРАВИЛЬНЫЙ ОТВЕТ!!!)
+! ========================================
+ */
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating : 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// // console.log(apartment);
+// // console.table(apartment);
+
+
+/*
+* - Задача 2 
+! ========================================
+ */
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // console.log(apartment);
+// // console.table(apartment);
+
+
+
+/*
+* - Задача 3 
+! ========================================
+ */
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// // Change code below this line
+// const aptRating = apartment.rating;
+// const aptDescr = apartment.descr;
+// const aptPrice = apartment.price;
+// const aptTags = apartment.tags;
+// // Change code above this line
+// console.log(aptRating);
+// console.log(aptDescr);
+// console.log(aptPrice);
+// console.log(aptTags);
+
+
+/*
+* - Задача 4 
+! ========================================
+ */
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+
+// // const lastTag = apartment.tags[2]; //! 1-ый вариант
+
+// // const last = apartment.tags.length - 1; //! 2-ый вариант
+// // console.log(last);                      //! 2-ый вариант
+// // const lastTag = apartment.tags[last];   //! 2-ый вариант
+
+// const lastTag = apartment.tags[apartment.tags.length - 1]; //! 3-ый вариант ПРАВИЛЬНЫЙ
+
+// // const lastTag = apartment.tags[length - 1]; //! НЕПРАВИЛЬНО
+
+
+// // Change code above this line
+// console.log(ownerName);
+// console.log(ownerPhone);
+// console.log(ownerEmail);
+// console.log(numberOfTags);
+// console.log(firstTag);
+// console.log(lastTag);
+
+
+/*
+* - Задача 5 
+! ========================================
+ */
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// // Change code below this line
+// const aptRating = apartment["rating"];
+// const aptDescr = apartment["descr"];
+// const aptPrice = apartment["price"];
+// const aptTags = apartment["tags"];
+// // Change code above this line
+// console.log(aptRating);
+// console.log(aptDescr);
+// console.log(aptPrice);
+// console.log(aptTags);
+
+
+/*
+* - Задача 6
+! ========================================
+ */
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name  = "Henry Sibola";
+// apartment.tags.push("trusted");
+
+// console.log(apartment.price);
+// console.log(apartment.rating);
+// console.log(apartment.owner.name);
+// console.log(apartment.tags);
+
+
+/*
+* - Задача 7
+! ========================================
+ */
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// apartment.area = 60;
+// apartment.rooms = 3;
+
+// // Добавление ОБЪЕКТА - tags1 со значеннием МАССИВ - aray //! +++ MY
+// // apartment.tags1 = {}                 //! +++ MY
+// // apartment.tags1.aray = []            //! +++ MY
+// // apartment.tags1.aray.push("jkhkjh")  //! +++ MY
+
+// apartment.location = {}
+// apartment.location.country = "Jamaica";
+// apartment.location.city = "Kingston";
+
+// console.log(apartment.area);
+// console.log(apartment.rooms);
+// console.log(apartment.location.country);
+// console.log(apartment.location.city);
+
+// console.log(apartment.tags1.aray);
+// console.log(apartment);
+
+/*
+* - Задача 8
+! ========================================
+ */
+
+// const name = "Repair Droid";
+// const price = 2500;
+// const image = "https://via.placeholder.com/640x480";
+// const tags = ["on sale", "trending", "best buy"];
+
+// const product = {
+//   // Change code below this line
+//   name,
+//   price,
+//   image,
+//   tags,
+//   // Change code above this line
+// };
+// // console.log(product);
+
+
+/*
+* - Задача 9
+! ========================================
+ */
+
+// const emailInputName = "email";
+// const passwordInputName = "password";
+
+// const credentials = {
+//   // Change code below this line
+//   [emailInputName]: "henry.carter@aptmail.com",
+//   [passwordInputName]: "jqueryismyjam",
+//   // Change code above this line
+// };
+
+
+/*
+* - Задача 10
+! ========================================
+ */
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+
+// for (const key in apartment) {
+//   // Ключ
+//   keys.push(key);
+//   // console.log(keys);
+//   // Значение свойства с таким ключом
+//   values.push(apartment[key]);
+//   // console.log(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
+
+/*
+* - Задача 11
+! ========================================
+ */
+
+// const keys = [];
+// const values = [];
+
+// const advert = {
+//   service: "apt",
+// };
+
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+//   // Change code above this line
+// }
+// console.log(keys);
+// console.log(values);
+
+
+/*
+* - Задача 12
+! ========================================
+ */
+
+function countProps(object) {
+  let propCount = 0;
+  // Change code below this line
+
+  // Change code above this line
+  return propCount;
+}
+
+console.log(countProps({}));
+console.log(countProps({ name: "Mango", age: 2 }));
+
---
+++ js/cm03-06.js
@@ -0,0 +1,351 @@
+/*
+* - Conspect Module - 3_6
+
+! ========================================
+ */
+
+// const temps = [14, -4, 25, 8, 11];
+
+// // В консоли будет массив
+// console.log(temps);
+// // ❌ Так не сработает, потому что передаём целый массив
+// console.log(Math.max(temps)); // NaN
+
+// const w = [...temps]; //! работает!
+// console.log(w);
+
+// // В консоли будет набор отдельных чисел
+// console.log(...temps);
+// // ✅ Распылим коллекцию элементов как отдельные аргументы
+// console.log(Math.max(...temps)); // 25
+
+/*
+* - 
+! ========================================
+ */
+
+// const temps = [14, -4, 25, 8, 11];
+// console.log(temps);
+
+// // Это точная, но независимая копия массива temps
+// const copyOfTemps = [...temps];
+// console.log(copyOfTemps); // [14, -4, 25, 8, 11]
+
+/*
+* - 
+! ========================================
+ */
+
+// const lastWeekTemps = [14, 25, 11];
+// const currentWeekTemps = [23, 17, 18];
+// const allTemps = [...lastWeekTemps, ...currentWeekTemps];
+// console.log(allTemps); // [14, 25, 11, 23, 17, 18]
+// console.log(typeof allTemps);
+
+/*
+* - 
+! ========================================
+ */
+
+// const first = { propA: 5, propB: 10 };
+// const second = { propC: 15 };
+// const third = { ...first, ...second };
+// console.log(third); // { propA: 5, propB: 10, propC: 15 }
+
+/*
+* - 
+! ========================================
+ */
+
+// const first = { propA: 5, propB: 10, propC: 50 };
+// const second = { propC: 15, propD: 20 };
+
+// const third = { ...first, ...second };
+// console.log(third); // { propA: 5, propB: 10, propC: 15, propD: 20 }
+
+// const fourth = { ...second, ...first };
+// console.log(fourth); // { propA: 5, propB: 10, propC: 50, propD: 20 }
+
+/*
+* - 
+! ========================================
+ */
+
+// const first = { propA: 5, propB: 10, propC: 50 };
+// const second = { propC: 15 };
+
+// const third = { propB: 20, ...first, ...second };
+// console.log(third); // { propA: 5, propB: 10, propC: 15 }
+
+// const fourth = { ...first, ...second, propB: 20 };
+// console.log(fourth); // { propA: 5, propB: 20, propC: 15 }
+
+// const fifth = { ...first, propB: 20, ...second };
+// console.log(fifth); // { propA: 5, propB: 20, propC: 15 }
+
+/*
+* - 
+! ========================================
+ */
+
+// function multiply(...args) {
+//   console.log(args); // массив всех аргументов
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+/*
+* - 
+! ========================================
+ */
+
+// function multiply(x, y, ...z) {
+//   console.log(x); // Значение первого аргумента
+//   console.log(y); // Значение второго аргумента
+//   console.log(z); // Массив остальных аргументов
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+/*
+* - 
+! ========================================
+ */
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// const accessType = book.isPublic ? "публичном" : "закрытом";
+// const message = `Книга ${book.title} автора ${book.author} с рейтингом ${book.rating} находится в ${accessType} доступе.`;
+// console.log(message);
+
+/*
+* - 
+! ========================================
+ */
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// // Деструктуризируем
+// const { title, author, isPublic, rating, coverImage } = book;
+// console.log(coverImage); // undefined
+
+// const accessType = isPublic ? "публичном" : "закрытом";
+// const message = `Книга ${title} автора ${author} с рейтингом ${rating} находится в ${accessType} доступе.`;
+// console.log(message);
+/*
+* - 
+! ========================================
+ */
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+// };
+
+// // Добавим картинку обложки если её нет в объекте книги
+// const {
+//   title,
+//   coverImage = "https://via.placeholder.com/640/480",
+//   author,
+// } = book;
+
+// console.log(title); // The Last Kingdom
+// console.log(author); // Bernard Cornwell
+// console.log(coverImage); // https://via.placeholder.com/640/480
+
+/*
+* - 
+! ========================================
+ */
+
+const firstBook = {
+  title: "The Last Kingdom",
+  coverImage:
+    "https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg",
+};
+
+// const {
+//   title: firstTitle,
+//   coverImage: firstCoverImage = "https://via.placeholder.com/640/480",
+// } = firstBook;
+
+// console.log(firstTitle); // The Last Kingdom
+// console.log(firstCoverImage); // https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg
+
+// console.log(firstBook);
+
+
+// const secondBook = {
+//   title: "Сон смешного человека",
+// };
+
+// const {
+//   title: secondTitle,
+//   coverImage: secondCoverImage = "https://via.placeholder.com/640/480",
+// } = secondBook;
+
+// console.log(secondTitle); // Сон смешного человека
+// console.log(secondCoverImage); // https://via.placeholder.com/640/480
+
+/*
+* - 
+! ========================================
+ */
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "На берегу спокойных вод",
+//     author: "Роберт Шекли",
+//     rating: 8.51,
+//   },
+// ];
+
+// for (const book of books) {
+//   console.log(book.title);
+//   console.log(book.author);
+//   console.log(book.rating);
+// }
+
+/*
+* - 
+! ========================================
+ */
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "На берегу спокойных вод",
+//     author: "Роберт Шекли",
+//     rating: 8.51,
+//   },
+// ];
+
+
+// // for (const book of books) {
+// //   const { title, author, rating } = book;
+
+// //   console.log(title);
+// //   console.log(author);
+// //   console.log(rating);
+// // }
+
+// for (const { title, author, rating } of books) {
+//   console.log(title);
+//   console.log(author);
+//   console.log(rating);
+// }
+
+/*
+* - 
+! ========================================
+ */
+
+// const user = {
+//   name: "Jacques Gluke",
+//   tag: "jgluke",
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+
+// const {
+//   name,
+//   tag,
+//   stats: { followers, views: userViews, likes: userLikes = 0 },
+// } = user;
+
+// console.log(name); // Jacques Gluke
+// console.log(tag); // jgluke
+// console.log(followers); // 5603
+// console.log(userViews); // 4827
+// console.log(userLikes); // 1308
+
+/*
+* - 
+! ========================================
+ */
+
+// const rgb = [200, 255, 100];
+// const [red, green, blue] = rgb;
+
+// console.log(`R:${red},G:${green},B:${blue}`); // "R:200,G:255,B:100"
+
+/*
+* - 
+! ========================================
+ */
+
+const rgb = [200, 255, 100];
+let red, green, blue;
+
+[red, green, blue] = rgb;
+
+console.log(`R:${red},G:${green},B:${blue}`); // "R:200,G:255,B:100"
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
