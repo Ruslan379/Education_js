ccf007931ab4ef3735ffb71cc4824f931eb5e831
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-06-23 21:38:50 +0300

    Work on Autocheck Module-3 +30

--- js/ac_m-03.js
+++ js/ac_m-03.js
@@ -397,12 +397,431 @@
 ! ========================================
  */
 
-function countTotalSalary(salaries) {
-  let totalSalary = 0;
-  // Change code below this line
+// function countTotalSalary(salaries) {
+//   console.log(salaries);
+//   let totalSalary = 0;
+//   // Change code below this line
+//   const values = Object.values(salaries);
+//   console.log(values);
 
+// for (const value of values) {
+//   totalSalary += value;
+// }
+//   // Change code above this line
+//   return totalSalary;
+// }
+
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+
+/*
+* - Задача 17
+! ========================================
+ */
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+
+//   console.log(color);
+
+//   console.log(color.hex);
+
+//   console.log(color.rgb);
+
+//   console.log("+++++++++++++++++");
+
+//   hexColors.push(color.hex); //!  РЕШЕНИЕ
+//   // console.log(hexColors);
+//   // console.log("_________________");
+
+//   rgbColors.push(color.rgb); //!  РЕШЕНИЕ
+//   // console.log(rgbColors);
+//   // console.log("_________________");
+// }
+
+// console.log("====================================");
+// console.log(hexColors);
+// console.log(rgbColors);
+// console.log("====================================");
+
+
+/*
+* - Задача 18
+! ========================================
+ */
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+  
+//   // console.log(product);
+  
+//   // console.log(product.name);
+
+//   // console.log(product.price);
+
+//   // console.log(product.quantity);
+//   // console.log("___________________________");
+//     if (product.name === productName) {
+//       return product.price
+//     }
+//   }
+// return null
+// }
+
+// console.log(getProductPrice("Radar"));
+// console.log(getProductPrice("Grip"));
+// console.log(getProductPrice("Scanner"));
+// console.log(getProductPrice("Droid"));
+// console.log(getProductPrice("Engine"));
+
+
+/*
+* - Задача 19
+! ========================================
+ */
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   if (products[0][propName] === undefined) {
+//     return []
+//   }
+
+//   const arr = [];
+
+//   for (const el of products) {
+//     arr.push(el[propName]);
+//   }
+
+//   return arr;
+//   // Change code above this line
+// }
+
+// console.log(getAllPropValues("name"));
+// console.log(getAllPropValues("quantity"));
+// console.log(getAllPropValues("price"));
+// console.log(getAllPropValues("category"));
+
+
+/*
+* - Задача 20
+! ========================================
+ */
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Пиши код ниже этой строки
+//   let totalPrice = 0;
+
+//   for (let i = 0; i < products.length; i += 1) {
+//     // const element = products[i];
+//     // console.log(element);
+//     // console.log(products[i].name);
+//     // console.log(productName);
+    
+//     if (products[i].name === productName) {
+//     totalPrice = products[i].price *  products[i].quantity
+//     // console.log(totalPrice); 
+//     // console.log("_____________________________");
+//     }
+    
+//   }
+//     return totalPrice;
+//   // Пиши код выше этой строки
+// }
+
+// console.log(calculateTotalPrice("Blaster"));
+// console.log(calculateTotalPrice("Radar"));
+// console.log(calculateTotalPrice("Droid"));
+// console.log(calculateTotalPrice("Grip"));
+// console.log(calculateTotalPrice("Scanner"));
+
+
+/*
+* - Задача 21
+! ========================================
+ */
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// // const yesterday = highTemperatures.yesterday;
+// // const today = highTemperatures.today;
+// // const tomorrow = highTemperatures.tomorrow;
+
+// const { yesterday, today, tomorrow } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+// console.log(meanTemperature);
+
+
+/*
+* - Задача 22
+! ========================================
+ */
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// // const yesterday = highTemperatures.yesterday;
+// // const today = highTemperatures.today;
+// // const tomorrow = highTemperatures.tomorrow;
+// // const icon = highTemperatures.icon;
+
+// const { yesterday, today, tomorrow, icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"} = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+// console.log(meanTemperature);
+
+
+/*
+* - Задача 23
+! ========================================
+ */
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// // const highYesterday = highTemperatures.yesterday;
+// // const highToday = highTemperatures.today;
+// // const highTomorrow = highTemperatures.tomorrow;
+// // const highIcon = highTemperatures.icon;
+
+// const {
+//   yesterday: highYesterday,
+//   today: highToday,
+//   tomorrow: highTomorrow,
+//   icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"
+// } = highTemperatures;
+
+
+// // Change code above this line
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+// console.log(meanTemperature);
+
+
+/*
+* - Задача 24
+! ========================================
+ */
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const { hex, rgb } of colors) {
+//   // const { hex, rgb } = color; //! 2-ой вариант
+
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+// console.log(hexColors);
+// console.log(rgbColors);
+
+
+/*
+* - Задача 25
+! ========================================
+ */
+
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+
+// // const highToday = forecast.today.high;
+// // const lowToday = forecast.today.low;
+// // const todayIcon = forecast.today.icon;
+
+// // const highTomorrow = forecast.tomorrow.high;
+// // const lowTomorrow = forecast.tomorrow.low;
+// // const tomorrowIcon = forecast.tomorrow.icon;
+
+// const {
+//   today: {
+//     low: lowToday,
+//     high: highToday,
+//     icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" },
+//   tomorrow: {
+//     low: lowTomorrow,
+//     high: highTomorrow,
+//     icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" },
+// } = forecast;
+
+
+/*
+* - Задача 26
+! ========================================
+ */
+
+// // Change code below this line
+// function calculateMeanTemperature(forecast) {
+
+//   const {
+//   today: {
+//     low: todayLow,
+//     high: todayHigh},
+//   tomorrow: {
+//     low: tomorrowLow,
+//     high: tomorrowHigh},
+// } = forecast;
+
+//   // const todayLow = forecast.today.low;
+//   // const todayHigh = forecast.today.high;
+//   // const tomorrowLow = forecast.tomorrow.low;
+//   // const tomorrowHigh = forecast.tomorrow.high;
+
+//   // Change code above this line
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+// console.log(calculateMeanTemperature({ today: { low: 28, high: 32 }, tomorrow: { low: 25, high: 29 } }));
+// console.log(calculateMeanTemperature({ today: {low: 37, high: 40}, tomorrow: {low: 33, high: 38} }));
+
+
+/*
+* - Задача 27
+! ========================================
+ */
+
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
+
+// console.log(bestScore);
+// console.log(worstScore);
+
+/*
+* - Задача 28
+! ========================================
+ */
+
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [...firstGroupScores,...secondGroupScores, ...thirdGroupScores ];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
+
+// console.log(allScores);
+// console.log(bestScore);
+// console.log(worstScore);
+
+
+/*
+* - Задача 29
+! ========================================
+ */
+
+// const defaultSettings = {
+//   theme: "light",
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+// // Change code below this line
+// const finalSettings = { ...defaultSettings, ...overrideSettings };
+// console.log(finalSettings);
+
+
+// // finalSettings.theme = "light"
+// // finalSettings.public = "false"
+// // finalSettings.minNumberOfQuestions = 10
+// // finalSettings.timePerQuestion = 30
+
+/*
+* - Задача 30
+! ========================================
+ */
+
+function makeTask(data) {
+  const completed = false;
+  const category = "General";
+  const priority = "Normal";
+  // Change code below this line
+  return {completed, category, priority, ...data} 
   // Change code above this line
-  return totalSalary;
 }
+
+console.log(makeTask({}));
+console.log(makeTask({ category: "Homemade", priority: "Low", text: "Take out the trash" }));
+console.log(makeTask({ category: "Finance", text: "Take interest" }));
+console.log(makeTask({ priority: "Low", text: "Choose shampoo" }));
+console.log(makeTask({ text: "Buy bread" }));
+
+
+/*
+* - Задача 31
+! ========================================
+ */
+
+
 
 
