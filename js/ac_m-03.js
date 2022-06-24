b5e39ac52fdf070855e5440b68bd76957b21b266
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-06-24 12:15:24 +0300

    Work on Autocheck Module-3 +40

--- js/ac_m-03.js
+++ js/ac_m-03.js
@@ -915,14 +915,202 @@
 ! ========================================
  */
 
-const bookShelf = {
+// const bookShelf = {
+//   // Change code below this line
+//   books: ["The last kingdom", "The guardian of dreams"],
+//   getBooks() {
+//     return "Returning all books";
+//   },
+
+//   addBook(bookName) {
+//     return `Adding book ${bookName}`;
+//   },
+
+//   removeBook(bookName) {
+//     return `Deleting book ${bookName}`;
+//   },
+
+//   updateBook(oldName, newName) {
+//     return `Updating book ${oldName} to ${newName}`;
+//   },
+//   // Change code above this line
+// };
+
+// console.log(bookShelf.getBooks());
+// console.log(bookShelf.addBook("Haze"));
+// console.log(bookShelf.removeBook("Red sunset"));
+// console.log(bookShelf.updateBook("Sands of dune", "Dune"));
+
+
+/*
+* - Задача 35
+! ========================================
+ */
+
+// const bookShelf = {
+//   books: ["The last kingdom", "Haze", "The guardian of dreams"],
+//   updateBook(oldName, newName) {
+
+//     // console.log(this.books);
+//     // console.log("oldName =", oldName);
+//     let oldNamePos = this.books.indexOf(oldName);
+//     // console.log("oldNamePos =", oldNamePos);
+//     // console.log("newName =", newName);
+//     this.books.splice(oldNamePos, 1, newName);
+//     // console.log(this.books);
+
+//     // Change code below this line
+
+
+//     return this.books;
+//     // Change code above this line
+//   },
+// };
+
+// console.log(bookShelf.updateBook("Haze", "Dungeon chronicles"));
+// console.log(bookShelf.updateBook("The last kingdom", "Dune"));
+
+
+/*
+* - Задача 36
+! ========================================
+ */
+
+// const atTheOldToad = {
+//   // Change code below this line
+//   potions: []  
+
+//   // Change code above this line
+// };
+
+// console.log(atTheOldToad.potions);
+
+
+/*
+* - Задача 37
+! ========================================
+ */
+
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   // Change code below this line
+
+//   getPotions() {
+//     return this.potions;
+//   },
+
+//   // Change code above this line
+// };
+// console.log(atTheOldToad.getPotions());
+
+
+/*
+* - Задача 38
+! ========================================
+ */
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   addPotion(potionName) {
+//     // Change code below this line
+//     this.potions.push(potionName);
+
+//     return this.potions;
+//     // Change code above this line
+//   },
+// };
+
+// console.log(atTheOldToad.addPotion("Invisibility"));
+// console.log(atTheOldToad.addPotion("Power potion"));
+
+
+/*
+* - Задача 39
+! ========================================
+ */
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   removePotion(potionName) {
+//     // Change code below this line
+
+//     let removePotionInd = this.potions.indexOf(potionName);
+//     // console.log(removePotionInd);
+//     this.potions.splice(removePotionInd, 1);
+
+//     return this.potions;
+
+//     // Change code above this line
+//   },
+// };
+
+// console.log(atTheOldToad.removePotion("Dragon breath"));
+// console.log(atTheOldToad.removePotion("Speed potion"));
+
+
+/*
+* - Задача 40
+! ========================================
+ */
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   updatePotionName(oldName, newName) {
+//     // Change code below this line
+
+//     let updatePotionInd = this.potions.indexOf(oldName);
+//     // console.log(updatePotionInd);
+
+//     this.potions.splice(updatePotionInd, 1, newName);
+
+//     return this.potions;
+//     // Change code above this line
+//   },
+// };
+
+// console.log(atTheOldToad.updatePotionName("Dragon breath", "Polymorth"));
+// console.log(atTheOldToad.updatePotionName("Stone skin", "Invisibility"));
+
+/*
+* - Задача 41
+! ========================================
+ */
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
   // Change code below this line
-  books: ["The last kingdom", "The guardian of dreams"],
-  getBooks() {
-    return "Returning all books";
+  getPotions() {
+    return this.potions;
   },
-  addBook(bookName) {
-    return `Adding book ${bookName}`;
+  addPotion(newPotion) {
+    if (this.potions.includes(newPotion)) {
+      return `Error! Potion ${newPotion} is already in your inventory!`;
+    }
+
+    this.potions.push(newPotion);
+  },
+  removePotion(potionName) {
+    const potionIndex = this.potions.indexOf(potionName);
+
+    if (potionIndex === -1) {
+      return `Potion ${potionName} is not in inventory!`;
+    }
+
+    this.potions.splice(potionIndex, 1);
+  },
+  updatePotionName(oldName, newName) {
+    const potionIndex = this.potions.indexOf(oldName);
+
+    if (potionIndex === -1) {
+      return `Potion ${oldName} is not in inventory!`;
+    }
+
+    this.potions.splice(potionIndex, 1, newName);
   },
   // Change code above this line
 };
