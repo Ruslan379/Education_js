fd419a6c70bb64cb4fb9d647430a9912a184dae5
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-01 23:22:54 +0300

    Started cm06-11

---
+++ js/cm06-11.js
@@ -0,0 +1,191 @@
+/*
+* - Conspect Module - 6_11
+
+! ========================================
+ */
+
+//! ___Навигация по DOM___
+
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const list = body.firstElementChild;
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);
+
+const listItems = list.children;
+console.log(listItems);
+
+/*
+* - 
+! ========================================
+ */
+
+//! ___Поиск элементов___
+
+const listWithId = document.querySelector('#menu');
+listWithId.style.textTransform = 'uppercase';
+listWithId.style.fontSize = '24px';
+console.log(listWithId);
+
+const listWithClass = document.querySelector('.menu');
+console.log(listWithClass);
+
+const menuItemsByTagName = document.querySelectorAll("li");
+console.log(menuItemsByTagName);
+
+const menuItemsByClass = document.querySelectorAll(".menu-item");
+console.log(menuItemsByClass);
+
+const firstMenuItem = document.querySelector(".menu-item");
+firstMenuItem.style.color = 'tomato';
+console.log(firstMenuItem);
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
+
+
+/*
+* - 
+! ========================================
+ */
+
