03b552908f7d9d35d3b9dd95aee37c2f5da4b8d2
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-02 17:05:05 +0300

    Working on cm06-11

--- js/cm06-11.js
+++ js/cm06-11.js
@@ -6,19 +6,29 @@
 
 //! ___Навигация по DOM___
 
-console.log(document);
+// // console.log(document);
+// console.log("🚀 document:", document)
 
-const body = document.body;
-console.log(body);
+// const body = document.body;
+// console.log("🚀 body = document.body", body)
+// // console.log(body);
 
-const list = body.firstElementChild;
-console.log(list);
+// const list = body.firstElementChild;
+// console.log("🚀 list = body.firstElementChild", list)
+// // console.log(list);
 
-const firstListItem = list.firstElementChild;
-console.log(firstListItem);
+// const firstListItem = list.firstElementChild;
+// console.log("🚀 firstListItem = list.firstElementChild", firstListItem)
+// // console.log(firstListItem);
 
-const listItems = list.children;
-console.log(listItems);
+// const listItems = list.children;
+// console.log("🚀 listItems = list.children", listItems)
+// // console.log(listItems);
+
+// console.log("================================================");
+// console.log("🚀 body.childNodes", body.childNodes);
+
+
 
 /*
 * - 
@@ -27,30 +37,44 @@ console.log(listItems);
 
 //! ___Поиск элементов___
 
-const listWithId = document.querySelector('#menu');
-listWithId.style.textTransform = 'uppercase';
-listWithId.style.fontSize = '24px';
-console.log(listWithId);
+// const listWithId = document.querySelector('#menu');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+// console.log("🚀 listWithId = document.querySelector('#menu'):", listWithId);
 
-const listWithClass = document.querySelector('.menu');
-console.log(listWithClass);
+// const listWithClass = document.querySelector('.menu');
+// console.log("🚀 listWithClass = document.querySelector('.menu'):", listWithClass);
 
-const menuItemsByTagName = document.querySelectorAll("li");
-console.log(menuItemsByTagName);
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName);
 
-const menuItemsByClass = document.querySelectorAll(".menu-item");
-console.log(menuItemsByClass);
+// const menuItemsByClass = document.querySelectorAll(".menu-item");
+// console.log(menuItemsByClass);
 
-const firstMenuItem = document.querySelector(".menu-item");
-firstMenuItem.style.color = 'tomato';
-console.log(firstMenuItem);
+// const firstMenuItem = document.querySelector(".menu-item");
+// firstMenuItem.style.color = 'tomato';
+// firstMenuItem.style.color = 'green';
+// console.log(firstMenuItem);
 
 /*
 * - 
 ! ========================================
  */
 
+//! ___Свойства и атрибуты___
 
+//===================================================
+const message = document.querySelector("#message");
+console.log(message.value); // Default textarea message
+
+//===================================================
+const activeLink = document.querySelector(".btn.active");
+console.log(activeLink.href); // https://s.codepen.io/about
+
+//===================================================
+const image = document.querySelector(".image");
+console.log(image.src); // https://placeimg.com/640/480/animals
+image.src = 'https://placeimg.com/640/480/tech';
 
 /*
 * - 
