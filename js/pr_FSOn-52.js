e1973927884238c442dceb3627a290dc2e518879
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-23 17:24:56 +0300

    men.pr. 23.07.2022

--- js/pr_FSOn-52.js
+++ js/pr_FSOn-52.js
@@ -18,7 +18,7 @@
 //     } else { alert("Не знаете? ECMAScript!"); };
 
 /*
-* - 
+* -
 ! ========================================
  */
 
@@ -38,7 +38,7 @@
 
 // if (ansver) {
 //     console.log(typeof ansver);
-    
+
 //     hours = String(Number.parseInt(ansver / 60)).padStart(2, 0); //! String.prototype.padStart(targetLength, [padString]) -
 //* - заполняет текущую строку(в начале(слева) текущей строки) другой строкой(несколько раз, если нужно) так, 
 //* что итоговая строка достигает заданной длины.
@@ -55,7 +55,7 @@
 // }
 
 /*
-* - 
+* -
 ! ========================================
  */
 
@@ -78,7 +78,7 @@
 // console.log(result);
 
 /*
-* - 
+* -
 ! ========================================
  */
 
@@ -97,7 +97,7 @@
 
 
 /*
-* - 
+* -
 ! ========================================
  */
 
@@ -110,7 +110,7 @@
 // //? =======================================
 
 /*
-* - 
+* -
 ! ========================================
  */
 //Создайте массив styles с элементами «Джаз» и «Блюз».
@@ -143,7 +143,7 @@
 // console.log(styles);
 
 /*
-* - 
+* -
 ! ========================================
  */
 
@@ -168,7 +168,7 @@
 //             counter += 1
 //             console.log(counter);
 //         }
-        
+
 //     }
 //     return sum / counter
 // }
@@ -176,26 +176,26 @@
 
 
 //! 2-ой вариант
-function caclculateAverage(...args) {
-    let sum = 0
-    let counter = args.length
-    // console.log(counter);
+// function caclculateAverage(...args) {
+//     let sum = 0
+//     let counter = args.length
+//     // console.log(counter);
 
-    for (const arg of args) {
-        // if (!isNaN(arg)) console.log(arg);
-        if (typeof arg !== "number") {
-            counter -= 1
-            continue
-        }
-          sum += arg
-    }
-    return sum / counter
-}
-console.log(caclculateAverage(1, 5, 8, 10, "4", "hkj"));
+//     for (const arg of args) {
+//         // if (!isNaN(arg)) console.log(arg);
+//         if (typeof arg !== "number") {
+//             counter -= 1
+//             continue
+//         }
+//         sum += arg
+//     }
+//     return sum / counter
+// }
+// console.log(caclculateAverage(1, 5, 8, 10, "4", "hkj"));
 
 
 /*
-* - 
+* -
 ! ========================================
  */
 
@@ -209,7 +209,7 @@ console.log(caclculateAverage(1, 5, 8, 10, "4", "hkj"));
 
 
 /*
-* - 
+* -
 ! ========================================
  */
 
@@ -226,7 +226,7 @@ console.log(caclculateAverage(1, 5, 8, 10, "4", "hkj"));
 
 
 /*
-* - 
+* -
 ! ========================================
  */
 
@@ -248,7 +248,7 @@ console.log(caclculateAverage(1, 5, 8, 10, "4", "hkj"));
 
 
 /*
-* - 
+* -
 ! ========================================
  */
 
@@ -269,7 +269,7 @@ console.log(caclculateAverage(1, 5, 8, 10, "4", "hkj"));
 
 
 /*
-* - 
+* -
 ! ========================================
  */
 
@@ -281,7 +281,7 @@ console.log(caclculateAverage(1, 5, 8, 10, "4", "hkj"));
 
 
 /*
-* - 
+* -
 ! ========================================
  */
 
@@ -293,25 +293,101 @@ console.log(caclculateAverage(1, 5, 8, 10, "4", "hkj"));
 
 
 /*
-* - 
+* -
 ! ========================================
  */
 
+//TODO =============23.07.2022 =================
+//1. Напишите две функции
+// letMeSeeYourName(callback) - спрашивает имя пользователя
+//через prompt и вызывает callback функцию
+//greet(name) - коллбек принимающий имя и логирующий в консоль
+//строку "Привет <name>"
+//Реализуй проверку, что prompt не пустой
 
+// letMeSeeYourName(greet)
+
+// function letMeSeeYourName(callback) {
+//     const name = prompt("Введите ИМЯ");
+//     if (!name) return
+//     // console.log(name);
+//     callback(name);
+// }
+
+// function greet(name) {
+//     console.log("Привет", name);
+// }
 
 /*
 * - 
 ! ========================================
  */
 
+//2. Напишите две функции
+//makeProduct(name, price, callback) - принимает
+//имя и цену товара, а так же callback.
+//Функция создает объект товара, добавляя ему уникальный
+//идентификатор в свойство id и вызывает callback
+//передавая ему созданный объект.
+//showProduct(product) - коллбек принимающий объект
+//продукта и логирующий его в консоль
 
+// makeProduct("Fila", 200, showProduct)
+
+// function makeProduct(name, price, callback) {
+//     const produktObj = {
+//         id: Math.random(),
+//         name,
+//         price,
+//     };
+//     callback(produktObj)
+//     // console.log(produktObj);
+// }
+
+// function showProduct(produktObj) {
+//     console.log(produktObj);
+// }
 
 /*
 * - 
 ! ========================================
  */
 
+//6. Напишите функцию each(array, callback), которая
+//первым параметром принимает массив, а вторым - функцию,
+//которая применится к каждому элементу массива.
+//Функция each должна вернуть новый массив, элементами
+//которого будут результаты вызова callback
+//callback функци должна умножать элементы на 2
 
+
+const array = [3, 5, 6, 34, 8, 83, 12, 34];
+
+console.log(each(array, multi));
+
+function each(array, callback) {
+    console.log(array);
+
+    //! 1 вариант
+    // const newArr = [];
+    // for (const el of array) {
+    //     newArr.push(callback(el));
+    // }
+    // console.log(newArr);
+    // return newArr
+
+    //! 2 вариант
+    // const newArr = array.map((el) => callback(el));
+    // return newArr
+
+    //! 3 вариант
+    return array.map(callback);
+}
+
+function multi(el) {
+    // console.log(el);
+    return el * 2
+}
 
 /*
 * - 
